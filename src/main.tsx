bc6d2ab0842f0236841f6e30f7a485feb1ff6219
もり <morimonn49@gmail.com>
2026-03-01 15:49:58 +0900

    refactor:ログを本番に送らずSentryde

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// src/main.tsx
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+// Sentryをインポート
+import * as Sentry from "@sentry/react";
+
+// Sentryを初期化
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+// 本番にエラーログすら送らないことで意図しないセキュリティ漏れを防ぐ
+if (import.meta.env.VITE_SENTRY_DSN && import.meta.env.PROD) {
+  console.error = () => {};
+  console.warn = () => {};
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
