b8d7e4bd86436d644c5770a6737a7185dea0479f
もり <morimonn49@gmail.com>
2026-03-01 15:49:58 +0900

    refactor:ログを本番に送らずSentryde

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,29 @@
-// 'vite' ではなく 'vitest/config' から読み込むように変更してください
-import { defineConfig } from 'vitest/config' 
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
-// これで { test: ... } と書いても怒られなくなります
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
     globals: true,
   },
-})
+  esbuild: {
+    // TS先生に「ここはエラーが出るって知ってるから大丈夫だよ」と伝えます
+    // @ts-expect-error: 'drop' might not be in the current Vite types but works at runtime
+    pure: ['console.log', 'console.info', 'console.debug'],
+    drop: ["debugger"],
+  },
+
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
